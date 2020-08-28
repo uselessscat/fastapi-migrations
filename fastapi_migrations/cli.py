f701e77dc4e53d5b6afdabbc697f87b34625b0ec
Ariel Carvajal <arie.cbpro@gmail.com>
2020-08-27 20:10:20 -0400

    Add cli commands

---
+++ fastapi_migrations/cli.py
@@ -0,0 +1,34 @@
+import typing as t
+
+from typer import Typer
+from fastapi_migrations import Migrations, MigrationsConfig
+
+
+class MigrationsCli(Typer):
+    def __init__(
+        self,
+        config: MigrationsConfig = None,
+        *,
+        name: t.Optional[str] = 'db',
+        help: t.Optional[str] = 'Database tools',
+        **kwargs: t.Any
+    ):
+        super().__init__(name=name, help=help, **kwargs)
+
+        self.migrations = Migrations(config or MigrationsConfig())
+        self.__register_commands()
+
+    def __register_commands(self) -> None:
+        self.command('init')(self.migrations.init)
+        self.command('revision')(self.migrations.revision)
+        self.command('migrate')(self.migrations.migrate)
+        self.command('upgrade')(self.migrations.upgrade)
+        self.command('downgrade')(self.migrations.downgrade)
+        self.command('edit')(self.migrations.edit)
+        self.command('merge')(self.migrations.merge)
+        self.command('show')(self.migrations.show)
+        self.command('history')(self.migrations.history)
+        self.command('heads')(self.migrations.heads)
+        self.command('branches')(self.migrations.branches)
+        self.command('current')(self.migrations.current)
+        self.command('stamp')(self.migrations.stamp)
